4f5ef6f03f336b2b3e9b7f525bf8797af6b785a9
Mateusz Wołoszyński <mateusz.michal.woloszynski@gmail.com>
2025-09-12 20:34:20 +0200

    test

---
+++ MagFlow.DAL/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagFlow.DAL
+{
+    public class Repository
+    {
+        public void Test()
+        {
+
+        }
+    }
+}
