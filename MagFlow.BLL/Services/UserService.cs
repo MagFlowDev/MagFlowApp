280a965e160f261a8d0e1e11f24374b6f54f4545
Mateusz Woloszynski <mateusz.woloszynski@doverfs.com>
2025-10-28 17:09:29 +0100

    db context factory (+DI in service registration)

---
+++ MagFlow.BLL/Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using MagFlow.BLL.Services.Interfaces;
+using MagFlow.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagFlow.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+    }
+}
---
+++ MagFlow.EF/CoreDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using MagFlow.Shared.Models.Settings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagFlow.EF
+{
+    public class CoreDbContextFactory : ICoreDbContextFactory
+    {
+        public CoreDbContext CreateDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CoreDbContext>();
+            optionsBuilder.UseSqlServer(AppSettings.ConnectionStrings.CoreDb);
+            return new CoreDbContext(optionsBuilder.Options);
+        }
+    }
+}
