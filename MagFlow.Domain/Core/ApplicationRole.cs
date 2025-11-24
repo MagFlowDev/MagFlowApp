e4d2faa9e0c67e5df991ec210979287b9a70de33
Mateusz Woloszynski <mateusz.woloszynski@doverfs.com>
2025-11-24 14:43:44 +0100

    application user-role relation

--- MagFlow.Domain/Core/ApplicationRole.cs
+++ MagFlow.Domain/Core/ApplicationRole.cs
@@ -9,5 +9,7 @@ namespace MagFlow.Domain.Core
     public class ApplicationRole : IdentityRole<Guid>
     {
         public string? Description { get; set; }
+
+        public ICollection<ApplicationUserRole> Users { get; set; }
     }
 }
---
+++ MagFlow.EF/Seeds/Core/RoleSeeder.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagFlow.EF.Seeds.Core
+{
+    public class RoleSeeder : ICoreSeeder
+    {
+        public void Seed(CoreDbContext context)
+        {
+            Task.Run(async () => await SeedAsync(context, CancellationToken.None));
+        }
+
+        public async Task SeedAsync(CoreDbContext context, CancellationToken cancellationToken)
+        {
+            bool seed = false;
+            var superAdminRole = await context.ApplicationRoles.FirstOrDefaultAsync(r => r.NormalizedName == "SUPERADMIN");
+            if (superAdminRole == null)
+            {
+                
+            }
+
+            if (seed)
+                await context.SaveChangesAsync();
+        }
+    }
+}
