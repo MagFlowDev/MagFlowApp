9f80c41262ee2edc92404aa660402bd8424ce028
Mateusz Wołoszyński <mateusz.michal.woloszynski@gmail.com>
2025-11-25 22:55:16 +0100

    LandingPage navigation

---
+++ MagFlow.Web/wwwroot/js/navigation.js
@@ -0,0 +1,8 @@
+﻿window.blazorScrollTo = (id) => {
+    const el = document.getElementById(id);
+    if (!el) return;
+    el.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+};
