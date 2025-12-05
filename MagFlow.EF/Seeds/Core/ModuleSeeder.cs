6e0868b1911da38a0ffe88a4b29726c4687958ad
Mateusz Woloszynski <mateusz.woloszynski@doverfs.com>
2025-12-05 16:58:51 +0100

    custom enumerator

--- MagFlow.EF/Seeds/Core/ModuleSeeder.cs
+++ MagFlow.EF/Seeds/Core/ModuleSeeder.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MagFlow.Domain.Core;
+using MagFlow.Shared.Models.Enumerators;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +21,20 @@ namespace MagFlow.EF.Seeds.Core
         {
             bool seed = false;
 
+            foreach(var module in Enumeration.GetAll<ModuleType>())
+            {
+                var dbModule = await context.Modules.FirstOrDefaultAsync(m => m.Name.Equals(module.Name));
+                if (dbModule == null)
+                {
+                    dbModule = new Module()
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = module.Name,
+                    };
+                    await context.ApplicationRoles.AddAsync(dbRole);
+                    seed = true;
+                }
+            }
 
             if (seed)
                 await context.SaveChangesAsync();
---
+++ MagFlow.Shared/Models/Enumerators/ModuleType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagFlow.Shared.Models.Enumerators
+{
+    public class ModuleType : Enumeration
+    {
+        public static ModuleType UsersModule = new(1, nameof(UsersModule));
+        public static ModuleType ContractorsModule = new(2, nameof(ContractorsModule));
+        public static ModuleType WaresModule = new(3, nameof(WaresModule));
+        public static ModuleType OrdersModule = new(4, nameof(OrdersModule));
+        public static ModuleType DocumentsModule = new(5, nameof(DocumentsModule));
+        public static ModuleType WarehousesModule = new(6, nameof(WarehousesModule));
+        public static ModuleType MachinesModule = new(7, nameof(MachinesModule));
+        public static ModuleType ProcessesModule = new(8, nameof(ProcessesModule));
+        public static ModuleType RaportsModule = new(9, nameof(RaportsModule));
+        public static ModuleType WorkTimeModule = new(10, nameof(WorkTimeModule));
+        public static ModuleType ProductionPlanModule = new(11, nameof(ProductionPlanModule));
+        public static ModuleType LogisticsModule = new(12, nameof(LogisticsModule));
+
+        public ModuleType(int id, string name) : base(id, name)
+        {
+        }
+    }
+}
