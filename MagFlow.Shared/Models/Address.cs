a139437a456c63e2c0ad1d98a11c0e12d1c127e8
Mateusz Woloszynski <mateusz.woloszynski@doverfs.com>
2026-02-23 16:04:55 +0100

    upgraded .net to 10, updated packages, added company settings and address

---
+++ MagFlow.Shared/Models/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MagFlow.Shared.Models
+{
+    [ComplexType]
+    public class Address
+    {
+        [Required]
+        public string Line1 { get; set; }
+        public string? Line2 { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+    }
+}
